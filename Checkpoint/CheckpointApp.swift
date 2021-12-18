8380e5192ae85a97af5ff5823e3c1b00fd8330dd
Linus Skucas <git@linus.sh>
2021-12-17 22:33:13 -0800

    Initial Commit

---
+++ Checkpoint/CheckpointApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckpointApp.swift
+//  Checkpoint
+//
+//  Created by Linus Skucas on 12/17/21.
+//
+
+import SwiftUI
+
+@main
+struct CheckpointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
