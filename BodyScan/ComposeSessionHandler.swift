5b8867ee6826cd4c9b5147a45e5e0f69ee075b84
Linus Skucas <git@linus.sh>
2023-06-09 23:45:33 -0700

    Create basic blocking

--- BodyScan/ComposeSessionHandler.swift
+++ BodyScan/ComposeSessionHandler.swift
@@ -6,6 +6,7 @@
 //
 
 import MailKit
+import SwiftUI
 
 class ComposeSessionHandler: NSObject, MEComposeSessionHandler {
 
@@ -16,69 +17,18 @@ class ComposeSessionHandler: NSObject, MEComposeSessionHandler {
     func mailComposeSessionDidEnd(_ session: MEComposeSession) {
         // Perform any cleanup now that the compose session is over.
     }
-    
-    // MARK: - Annotating Address Tokens
-
-    func annotateAddressesForSession(_ session: MEComposeSession) async -> [MEEmailAddress: MEAddressAnnotation] {
-        var annotations: [MEEmailAddress: MEAddressAnnotation] = [:]
-        
-        // Iterate through all the receipients in the message.
-        //
-        // NOTE: If there isn't any definitive information we can provide about
-        // the address, don't add an annotation for it.
-        for address in session.mailMessage.allRecipientAddresses {
-            // If the address ends in @example.com, indicate that's an error.
-            if address.rawString.hasSuffix("@example.com") {
-                // Create an address annotation that Mail shows in the address token.
-                let message = "example.com is not a valid domain"
-                let annotation = MEAddressAnnotation.error(withLocalizedDescription: message)
-                
-                // Add the annotation to the results dictionary.
-                annotations[address] = annotation
-            }
-        }
-        
-        // Call the Completion handler with the results.
-        return annotations
-    }
 
     // MARK: - Displaying Custom Compose Options
 
     func viewController(for session: MEComposeSession) -> MEExtensionViewController {
-        return ComposeSessionViewController(nibName: "ComposeSessionViewController", bundle: Bundle.main)
-    }
-    
-    // MARK: - Adding Custom Headers
-
-    func additionalHeaders(for session: MEComposeSession) -> [String : [String]] {
-        // To insert custom headers into a message, return a dictionary with
-        // the key and an array of one or more values.
-        return ["X-CustomHeader": ["This ia a custom header."]]
-    }
-    
-    // MARK: - Confirming Message Delivery
-
-    enum ComposeSessionError: LocalizedError {
-        case invalidRecipientDomain
-        
-        var errorDescription: String? {
-            switch self {
-            case .invalidRecipientDomain:
-                return "example.com is not a valid recipient domain"
-            }
-        }
+        let viewController = MEExtensionViewController()
+        viewController.view = NSHostingView(rootView: Text("Hello, World!"))
+        return viewController
     }
     
     func allowMessageSendForSession(_ session: MEComposeSession, completion: @escaping (Error?) -> Void) {
-        // Before Mail sends a message, your extension can validate the
-        // contents of the compose session. If the message is ready to be sent,
-        // call the compltion block with nil. If the message isn't ready to be
-        // sent, call the completion with an error.
-        if session.mailMessage.allRecipientAddresses.contains(where: { $0.rawString.hasSuffix("@example.com")}) {
-            completion(ComposeSessionError.invalidRecipientDomain)
-        } else {
-            completion(nil)
-        }
+        let checkpointError = MEComposeSessionError(MEComposeSessionError.Code.invalidBody, userInfo: [NSLocalizedDescriptionKey: "Checkpoint! Confirm that you want to send this."])
+        completion(checkpointError)
     }
 }
 
